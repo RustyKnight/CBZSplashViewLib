50c57d6c187f81e99ae082ff95d5c4a88f6bfe45
Shane Whitehead <shane.whitehead@beamcommunications.com>
2016-10-04 15:22:32 +1100

    Initial commit

---
+++ CBZSplashViewLib/CBZSplashViewLib.h
@@ -0,0 +1,20 @@
+//
+//  CBZSplashViewLib.h
+//  CBZSplashViewLib
+//
+//  Created by Shane Whitehead on 4/10/2016.
+//  Copyright © 2016 Beam Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CBZSplashViewLib.
+FOUNDATION_EXPORT double CBZSplashViewLibVersionNumber;
+
+//! Project version string for CBZSplashViewLib.
+FOUNDATION_EXPORT const unsigned char CBZSplashViewLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBZSplashViewLib/PublicHeader.h>
+#import "CBZRasterSplashView.h"
+#import "CBZSplashView.h"
+#import "CBZVectorSplashView.h"
